f2b08e06fed0e3be4de865d5b6eba206527405bb
vidya sagar <sagarvidya2956@gmail.com>
2025-08-06 02:03:28 +0530

    Add files via upload

---
+++ positive or negative.py
@@ -0,0 +1,9 @@
+#this program is to take input from user and print the number is positive or negative
+
+x=int(input("enter a number :"))
+if x>0:
+    print(x,"is positive ")
+elif x<0:
+    print(x,"is negative")
+else:
+    print(x,"is neither positive nor negative")
