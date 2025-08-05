1ed21de30ef493f93f8c69e4d6f8e05cac505555
vidya sagar <sagarvidya2956@gmail.com>
2025-08-06 02:10:31 +0530

    Add files via upload

---
+++ maximum among two number.py
@@ -0,0 +1,27 @@
+# write a program to input two number and find maximum among them
+
+x=int(input("enter first number:"))
+y=int(input("enter second number :"))
+if (x>y):
+    print(x,"is maximum")
+elif(x<y):
+    print(y,"is maximum")
+else:
+    print("both are equal")
+
+
+'''
+output
+
+enter first number:2
+enter second number :2
+both are equal
+
+
+enter first number:33
+enter second number :66
+66 is maximum
+
+'''
+
+
