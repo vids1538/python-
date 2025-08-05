6a3ba5727aa70bf8414771016b9d801537d55ec3
vidya sagar <sagarvidya2956@gmail.com>
2025-08-06 01:05:06 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,15 @@
+#this program is to calculate the area of a circle
+
+r=float(input("enter a radious:"))
+pi=3.14
+result=pi*r*r
+print("are of circle is :",result,"metre square")
+
+
+'''  output
+
+enter a radious:4
+are of circle is : 50.24 metre square
+
+
+'''
