f20acd476f86c0b36bc4f6c2d9e08600bb1337de
vidya sagar <sagarvidya2956@gmail.com>
2025-08-05 23:27:05 +0530

    type conversion and checking keyboard library

---
+++ keyword library.py
@@ -0,0 +1,5 @@
+import keyword
+print(keyword.kwlist)
+print(keyword.iskeyword)
+print(keyword.issoftkeyword)
+print(keyword.softkwlist)
---
+++ type conversion.py
@@ -0,0 +1,25 @@
+#this program is to type conversion of data
+
+x="123"
+print(x)
+print(type(x))
+
+y=int(x)
+print(type(y))
+
+z=float(x)
+print(y)
+print(type(z))
+print(z)
+
+
+'''
+          outputs:-
+123
+<class 'str'>
+<class 'int'>
+123
+<class 'float'>
+123.0
+
+'''
