b86e876ba05cef27a33298a1dc698187f242e012
vidya sagar <sagarvidya2956@gmail.com>
2025-08-06 18:33:49 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,108 @@
+#this program is to learn for loop
+''' for loop has a function named range
+range has 3 types
+1. range(stop)
+2. range(start,stop)
+3. range (start, stop,step)
+'''
+x=[11,22,33,44,55,66,77,88,99]
+for i in x:
+    print(i)
+
+    
+y='raju'
+for i in y:
+    print(i,end='')
+
+print('\n')
+for i in range(4):
+    print("hello")
+
+for i in range(1,11):
+    print(i)
+
+print("all even numners between 1 to 100 :")
+for i in range(2,101,2):
+    print(i)
+
+
+'''
+11
+22
+33
+44
+55
+66
+77
+88
+99
+raju
+
+hello
+hello
+hello
+hello
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+all even numners between 1 to 100 :
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+22
+24
+26
+28
+30
+32
+34
+36
+38
+40
+42
+44
+46
+48
+50
+52
+54
+56
+58
+60
+62
+64
+66
+68
+70
+72
+74
+76
+78
+80
+82
+84
+86
+88
+90
+92
+94
+96
+98
+100
+'''
+    
+    
