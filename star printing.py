93ae5f0719841fe98ce7446feed8f45b87d3fbb9
vidya sagar <sagarvidya2956@gmail.com>
2025-08-09 17:59:28 +0530

    Update star printing.py
    added an additional pattern and it's code at last

--- star printing.py
+++ star printing.py
@@ -68,3 +68,16 @@ for i in range(1,6):
         else:
             print('',end='')
     print()
+'''
+   *
+  ***
+ *****
+*******
+'''
+for i in range(1,5):
+    for j in range(1,8):
+        if(j>=5-i and j<=3+i):
+            print("*",end='')
+        else:
+            print('',end='')
+    print()
